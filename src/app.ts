62a8e5bf273a03709b23d15f67c73409ffd72f0c
spacerr <kverenchkhiladzeirakli.personal@gmail.com>
2024-08-05 19:13:02 +0400

    init

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const TEXT = process.env.TEXT || 'Hello World!';
+
+
+app.get('/', (_, res) => {
+  res.send(TEXT);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
